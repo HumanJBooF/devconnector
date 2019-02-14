f790d6109e7a9b9d0b67fc379116c13b05b454e7
humanjboof <humanjboof@gmail.com>
2019-02-14 17:21:07 -0500

    heroku

--- server.js
+++ server.js
@@ -33,9 +33,9 @@ useJWT(passport);
 app.use('/', routes);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     })
 }
 
