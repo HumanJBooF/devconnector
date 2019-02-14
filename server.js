dfe9158a6668838cfcf97a2acdf51b679d089c1a
humanjboof <humanjboof@gmail.com>
2019-02-13 22:58:45 -0500

    lets go heroku, be a pal

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const routes = require('./routes');
 const path = require('path');
+const routes = require('./routes');
 
 const app = express();
 
@@ -32,12 +32,11 @@ useJWT(passport);
 // Routes
 app.use('/', routes);
 
+// Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    // Set static folder
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
 }
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
