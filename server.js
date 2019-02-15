529c2f6d6d1307a90c4759f8425af65a2e6497b9
humanjboof <humanjboof@gmail.com>
2019-02-15 13:08:48 -0500

    test

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
+
 const routes = require('./routes');
 
 const app = express();
@@ -32,7 +34,11 @@ useJWT(passport);
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('build'));
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 // Routes
